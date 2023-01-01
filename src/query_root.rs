dde22753623a55b16b7e315e16710c261a98fa07
sbaumohl <28550422+sbaumohl@users.noreply.github.com>
2023-01-01 13:37:00 -0600

    customized query options

--- src/query_root.rs
+++ src/query_root.rs
@@ -1,8 +1,9 @@
 use async_graphql::{Context, Object};
 use sea_orm::{
-    sea_query::Expr, sea_query::Func, Condition, DatabaseConnection, DbErr, EntityTrait,
-    QueryFilter, QuerySelect,
+    sea_query::Expr, sea_query::Func, ColumnTrait, Condition, DatabaseConnection, DbErr,
+    EntityTrait, QueryFilter, QuerySelect,
 };
+use seaography::EntityFilter;
 
 use crate::entities::{prelude::*, *};
 
@@ -17,28 +18,31 @@ pub struct QueryRoot;
 
 #[Object]
 impl QueryRoot {
-    async fn search(&self) -> Result<String, DbErr> {
-        return Ok("Hello, There!".to_string());
-    }
-
     async fn committees(
         &self,
         ctx: &Context<'_>,
-        q: Option<String>,
+        search: Option<String>,
         limit: Option<u64>,
+        filters: Option<committees::Filter>,
     ) -> Result<Vec<committees::Model>, DbErr> {
         let db = ctx.data::<DatabaseConnection>().unwrap();
 
         // conditional filter (search)
-        let mut filters = Condition::all();
+        let mut cond_filters = Condition::all();
 
-        if let Some(query) = q {
+        if let Some(query) = search {
             let like = format!("%{}%", query.to_lowercase());
-            filters = filters
+            cond_filters = cond_filters
                 .add(Expr::expr(Func::lower(Expr::col(committees::Column::Name))).like(like));
         }
 
-        let mut results = Committees::find().filter(filters);
+        // apply filters and search
+        let mut results = Committees::find().filter(cond_filters);
+
+        // Filter Object; apply filters and search only if filters were given
+        if let Some(filters) = filters {
+            results = results.filter(filters.filter_condition());
+        }
 
         // limit returned no.
         if let Some(lim) = limit {
@@ -47,6 +51,52 @@ impl QueryRoot {
 
         // return results
         results.all(db).await
+    }
+
+    async fn organizations(
+        &self,
+        ctx: &Context<'_>,
+        search: Option<String>,
+        limit: Option<u64>,
+        filters: Option<committees::Filter>,
+    ) -> Result<Vec<String>, DbErr> {
+        let db = ctx.data::<DatabaseConnection>().unwrap();
+
+        // conditional filter (search)
+        let mut cond_filters = Condition::all();
+
+        // we want to exclude "NONE" or "null" connectedOrg results
+        cond_filters = cond_filters
+            .add(committees::Column::ConnectedOrg.is_not_null())
+            .add(committees::Column::ConnectedOrg.ne("NONE"));
+
+        if let Some(query) = search {
+            println!("{:?}", query);
+            let like = format!("%{}%", query.to_lowercase());
+            cond_filters = cond_filters.add(
+                Expr::expr(Func::lower(Expr::col(committees::Column::ConnectedOrg))).like(like),
+            );
+        }
+
+        let mut results = Committees::find().filter(cond_filters);
+
+        // Filter object; check if there are filters and apply (TODO maybe remove?)
+        results = if let Some(f) = filters {
+            results.filter(f.filter_condition())
+        } else {
+            results
+        };
+
+        // limit returned no.
+        if let Some(lim) = limit {
+            results = results.limit(lim);
+        }
+
+        return results.all(db).await.and_then(|f| {
+            Ok(f.iter()
+                .map(|f: &committees::Model| f.connected_org.clone().unwrap())
+                .collect())
+        });
     }
 
     async fn members(&self, ctx: &Context<'_>) -> Result<Vec<members::Model>, DbErr> {
